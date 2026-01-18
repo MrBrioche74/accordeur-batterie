9e7862a8a9eb5c65129f5c9aebe6dbaab870efbe
MrBrioche74 <a.depecker@gmail.com>
2026-01-18 11:48:42 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE = "drum-tuner-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.webmanifest",
+  "./icon.svg"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => k === CACHE ? null : caches.delete(k))))
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
